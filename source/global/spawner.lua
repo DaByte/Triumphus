fd61decfc011aab8ef93d8240f7174ba94ff4157
Mark Otaris <mark.otaris@gmail.com>
2013-10-08 22:35:18 -0400

    converted spawner module documentation to be included in the code instead of at the head of it

--- source/global/spawner.lua
+++ source/global/spawner.lua
@@ -1,40 +1,15 @@
 --- The spawner package provides replacement functionality for character spawning, which is normally overridden by Cure. This is not available to clients.
---
--- <h2>API</h2>
---
--- - `spawner.Add ( player )`
---
--- 	Adds a player to the spawner. Usually used with Player.PlayerAdded.
---
--- - `spawner.Remove ( player )`
---
--- 	Removes a player from the spawner. Usually used with
--- 	Player.PlayerRemoving.
---
--- - `spawner.Settings`
---
--- 	A table of settings. Contains the following values:
---
--- 	- CharacterAutoLoads
---
--- 		A bool indicating whether the character of a player will load
--- 		automatically. Use this setting in place of
--- 		`Players.CharacterAutoLoads`. Defaults to `true`.
---
--- 	- RespawnCooldown
---
--- 		A number indicating the amount of time to wait after a character dies
--- 		before respawning, in seconds. Defaults to 5.
 -- module: spawner
-
 
 if IsClient then
 	return nil
 end
 
+--- A table of settings.
+-- table: Settings
 local settings = {
-	CharacterAutoLoads = true;
-	RespawnCooldown = 5;
+	CharacterAutoLoads = true; -- A bool indicating whether the character of a player will load automatically. Use this setting in place of `Players.CharacterAutoLoads`. Defaults to `true`.
+	RespawnCooldown = 5; -- A number indicating the amount of time to wait after a character dies before respawning, in seconds. Defaults to 5.
 }
 
 local function getHumanoid(character)
@@ -47,8 +22,10 @@ local function getHumanoid(character)
 	return nil
 end
 
-local addedPlayers = {}
+local addedPlayers
 
+--- Removes a player from the spawner. Usually used with `Players.PlayerRemoving`.
+-- !Player: player
 local function Remove(player)
 	if addedPlayers[player] then
 		addedPlayers[player]:disconnect()
@@ -56,6 +33,8 @@ local function Remove(player)
 	end
 end
 
+--- Adds a player to the spawner. Usually used with `Player.PlayerAdded`.
+-- !Player: player
 local function Add(player)
 	local function respawn()
 		if not settings.CharacterAutoLoads then return end
